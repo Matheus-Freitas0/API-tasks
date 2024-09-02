58c0d9ff8ae2bd2a339e9df402a1de1ed27743f7
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-02 18:19:30 -0300

    update controller

--- src/controllers/tarefaController.js
+++ src/controllers/tarefaController.js
@@ -1,22 +1,22 @@
-const TarefaRepository = require('../repositories/tarefaRepository');
+const TarefaRepository = require('../repositories/tarefaRepository')
 
 class TarefaController {
     async getAllTarefas(req, res) {
         try {
-            const tarefas = await TarefaRepository.getAllTarefas();
-            res.json(tarefas);
+            const tarefas = await TarefaRepository.getAllTarefas()
+            res.json(tarefas)
         } catch (error) {
-            res.status(500).json({ error: error.message });
+            res.status(500).json({ error: error.message })
         }
     }
     async createTarefa(req, res) {
-        const { titulo, descricao, concluida } = req.body;
+        const { titulo, descricao, concluida } = req.body
         try {
             const novaTarefa = await TarefaRepository.createTarefa
-                ({ titulo, descricao, concluida });
-            res.status(201).json(novaTarefa);
+                ({ titulo, descricao, concluida })
+            res.status(201).json(novaTarefa)
         } catch (error) {
-            res.status(500).json({ error: error.message });
+            res.status(500).json({ error: error.message })
         }
     }
 

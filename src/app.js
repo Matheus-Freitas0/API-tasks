8003af05839ba45a2118db23eddd09cb7752f2de
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-05 15:05:08 -0300

    Added CORS headers to app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,13 @@ const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use('/tarefas', tarefasRouter);
 
 sequelize.sync()

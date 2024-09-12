d06fea4793c21d0a8c7b2ae66e6f54a40044a63e
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-11 21:22:44 -0300

    Added authentication and validation to tarefas routes

--- src/routes/tarefas.js
+++ src/routes/tarefas.js
@@ -8,11 +8,9 @@ router.get('/', (req, res) => tarefaController.getAllTarefas
 router.get('/:id', (req, res) => tarefaController.getByIdTarefa
     (req, res))
 
-router.put('/:id', (req, res) => tarefaController.updateEditTarefa
-    (req, res))
+router.put('/:id', authenticateToken, validateRequest(tarefaSchema), (req, res) => tarefaController.updateEditTarefa(req, res));
 
-router.put('/concluida/:id', (req, res) => tarefaController.updateConcluirTarefa
-    (req, res)) 
+router.put('/concluida/:id', authenticateToken, (req, res) => tarefaController.updateConcluirTarefa(req, res));
 
 router.post('/byIds', (req, res) => tarefaController.getByIdsTarefas
     (req, res));
@@ -23,10 +21,8 @@ router.post('/', (req, res) => tarefaController.createTarefa
 router.put('/:id', (req, res) => tarefaController.updateTarefa
     (req, res))
 
-router.delete('/:id', (req, res) => tarefaController.deleteTarefa
-    (req, res))
+router.delete('/:id', authenticateToken, (req, res) => tarefaController.deleteTarefa(req, res));
 
-router.delete('/', (req, res) => tarefaController.deleteManyTarefas
-    (req, res))
+router.delete('/', authenticateToken, (req, res) => tarefaController.deleteManyTarefas(req, res));
 
 module.exports = router

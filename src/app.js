8ec1e84773a8c17c4065cc1a9d3b1607d895f943
MatheusFreitas <moncaomatheus@gmail.com>
2024-09-16 14:58:12 -0300

    "Removed authenticateToken, validateTarefa, and errorHandler middlewares from app.js"

--- src/app.js
+++ src/app.js
@@ -2,9 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./config/database');
 const tarefasRouter = require('./routes/tarefas');
-const authenticateToken = require('./middlewares/authMiddleware');
-const validateTarefa = require('./middlewares/validationMiddleware');
-const errorHandler = require('./middlewares/errorMiddleware');
 const app = express();
 const port = 3000;
 
@@ -16,16 +13,10 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
 
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
   next();
 });
 
-app.use('/tarefas', authenticateToken, validateTarefa, tarefasRouter);
-
-app.use(errorHandler);
+app.use('/tarefas', tarefasRouter);
 
 sequelize.sync()
   .then(() => {
